c1bf93d7575ec5afb792029c5aabff3a89b9e2f5
gittttt1488 <pkanteepob4@gmail.com>
2025-07-08 09:40:26 +0700

    Update pxt.json, main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,10 +1,5 @@
-input.onButtonPressed(Button.A, function () {
-	
-})
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P0) == 1) {
-    	
-    } else if (false) {
-    	
+        music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
     }
 })
