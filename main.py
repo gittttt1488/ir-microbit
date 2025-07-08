02516428ada08eb21147a3fec7af1449f0dfaa0a
gittttt1488 <pkanteepob4@gmail.com>
2025-07-08 09:52:25 +0700

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-
+def on_forever():
+    if pins.digital_read_pin(DigitalPin.P0) == 1:
+        basic.show_leds("""
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            """)
+    else:
+        if pins.digital_read_pin(DigitalPin.P1) == 1:
+            basic.show_leds("""
+                . . # . .
+                . . . # .
+                # # # # #
+                . . . # .
+                . . # . .
+                """)
+        else:
+            pass
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,5 +1,23 @@
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P0) == 1) {
-        music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else {
+        if (pins.digitalReadPin(DigitalPin.P1) == 1) {
+            basic.showLeds(`
+                . . # . .
+                . . . # .
+                # # # # #
+                . . . # .
+                . . # . .
+                `)
+        } else {
+        	
+        }
     }
 })
