cbfa028cab1d0a2e9c28be2ea998d6e80db7cc1e
gittttt1488 <pkanteepob4@gmail.com>
2025-07-08 09:32:20 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P0) == 1) {
+    	
+    } else if (false) {
+    	
+    }
+})
